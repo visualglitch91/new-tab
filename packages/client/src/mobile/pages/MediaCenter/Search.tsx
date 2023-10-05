b4b798940c03298350d433bcea7c38016c9cd587
visualglitch91 <visualglitch91@proton.me>
2023-10-05 10:43:34 -0300

    Blur search field when scrolling

--- packages/client/src/mobile/pages/MediaCenter/Search.tsx
+++ packages/client/src/mobile/pages/MediaCenter/Search.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useId, useRef, useState } from "react";
 import { Box, TextField, alpha } from "@mui/material";
 import PageLayout from "../../components/PageLayout";
 import PageTile from "../../components/PageTitle";
 import Search from "../../../components/MediaCenter/Search";
+import useMountEffect from "../../../utils/useMountEffect";
 
 export default function SearchPage() {
+  const id = useId();
+  const searchFieldId = `${id}-search-field`;
+  const inputWrapperRef = useRef<HTMLDivElement>(null);
   const [term, setTerm] = useState("");
   const [delayedTerm, setDelayedTerm] = useState(term);
 
@@ -13,17 +17,36 @@ export default function SearchPage() {
     return () => clearTimeout(timeout);
   }, [term]);
 
+  useMountEffect(() => {
+    const onScroll = () => {
+      Array.from(
+        document.querySelectorAll<HTMLInputElement>(
+          `[data-input-id="${searchFieldId}"] input`
+        )
+      ).forEach((it) => {
+        it.blur();
+      });
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  });
+
   return (
     <PageLayout
       header={
         <>
           <PageTile>Busca</PageTile>
-          <Box pt="12px" pb="4px" width="100%">
+          <Box pt="12px" pb="4px" width="100%" ref={inputWrapperRef}>
             <TextField
               fullWidth
               size="small"
               placeholder="Digite para pesquisar..."
               value={term}
+              data-input-id={searchFieldId}
               onChange={(e) => setTerm(e.currentTarget.value)}
               sx={(theme) => ({
                 "& .MuiOutlinedInput-notchedOutline": {
