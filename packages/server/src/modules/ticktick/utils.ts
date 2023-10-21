c051c538e5a2611570f110d6e18b2b80fd85fb0d
visualglitch91 <visualglitch91@proton.me>
2023-10-21 12:41:28 -0300

    Fix all day events

--- packages/server/src/modules/ticktick/utils.ts
+++ packages/server/src/modules/ticktick/utils.ts
@@ -1,8 +1,11 @@
-import { parse } from "date-fns";
+import { parse, subMilliseconds } from "date-fns";
 
 export function normalizeDate(date: string, isAllDay = false) {
   if (isAllDay) {
-    return parse(date.substring(0, 10), "yyyy-MM-dd", new Date());
+    return subMilliseconds(
+      parse(date.substring(0, 10), "yyyy-MM-dd", new Date()),
+      1
+    );
   }
 
   return new Date(date);
